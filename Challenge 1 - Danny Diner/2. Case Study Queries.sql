a51867cab79589d800e0dc51b781300ba83a1a85
Lan <hong-lan.hoang@etu.unige.ch>
2023-04-21 00:17:51 +0200

    Update 2. Case Study Queries.sql

--- Challenge 1 - Danny Diner/2. Case Study Queries.sql
+++ Challenge 1 - Danny Diner/2. Case Study Queries.sql
@@ -42,22 +42,24 @@ GROUP BY s.product_id, me.product_name
 ORDER BY volume_sold DESC;
 
 /*5. Which item was the most popular for each customer?
-(approach: find the number of products bought by each customer
-then get the product with highest number of each customer
+(approach: find the number of products bought by each customer, assign rank based on number of products bought
+then get the product with highest number of each customer based on the first rank
 */
 WITH product_count AS (
-	SELECT customer_id,  me.product_name, COUNT(s.product_id) "prod_count"
+	SELECT customer_id,  me.product_name, COUNT(s.product_id) "prod_count",
+		DENSE_RANK() OVER(PARTITION BY s.customer_id
+      	ORDER BY COUNT(s.product_id) DESC) AS rank
 	FROM sales AS s
 	INNER JOIN menu AS me
 	ON s.product_id = me.product_id
 	GROUP BY me.product_name, customer_id
-	ORDER BY customer_id, prod_count DESC
 )
-SELECT customer_id, product_name, MAX(prod_count)
+SELECT customer_id, product_name, prod_count
 FROM product_count
-GROUP BY customer_id, product_name
+WHERE rank = 1;
 
 /*6. Which item was purchased first by the customer after they became a member?*/
+
 /*7. Which item was purchased just before the customer became a member?*/
 /*8. What is the total items and amount spent for each member before they became a member?*/
 /*9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?*/
