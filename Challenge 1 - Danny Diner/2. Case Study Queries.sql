ce764e44b7b2822aa83339da2999a125534e2254
Lan <hong-lan.hoang@etu.unige.ch>
2023-04-21 11:56:15 +0200

    Update 2. Case Study Queries.sql

--- Challenge 1 - Danny Diner/2. Case Study Queries.sql
+++ Challenge 1 - Danny Diner/2. Case Study Queries.sql
@@ -59,9 +59,50 @@ FROM product_count
 WHERE rank = 1;
 
 /*6. Which item was purchased first by the customer after they became a member?*/
+WITH member_order_cte AS (	
+	SELECT s.customer_id, s.order_date, me.product_name, 
+	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY order_date) AS date_rank
+	FROM sales AS s
+	INNER JOIN members AS mb
+		ON s.customer_id = mb.customer_id
+	INNER JOIN menu AS me
+		ON s.product_id = me.product_id
+	WHERE s.order_date >= mb.join_date
+)
+SELECT customer_id, order_date, product_name
+FROM member_order_cte
+WHERE date_rank = 1;
+
 
 /*7. Which item was purchased just before the customer became a member?*/
+WITH order_bf_member_cte AS (
+	SELECT s.customer_id, s.order_date, me.product_name, 
+	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY order_date DESC) AS date_rank
+	FROM sales AS s
+	INNER JOIN members AS mb
+		ON s.customer_id = mb.customer_id
+	INNER JOIN menu AS me
+		ON s.product_id = me.product_id
+	WHERE s.order_date < mb.join_date
+)
+SELECT customer_id, order_date, product_name
+FROM order_bf_member_cte
+WHERE date_rank = 1;
+
 /*8. What is the total items and amount spent for each member before they became a member?*/
+WITH order_bf_member_cte AS (
+	SELECT s.customer_id, s.order_date, me.product_name, price,
+	DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY order_date DESC) AS date_rank
+	FROM sales AS s
+	INNER JOIN members AS mb
+		ON s.customer_id = mb.customer_id
+	INNER JOIN menu AS me
+		ON s.product_id = me.product_id
+	WHERE s.order_date < mb.join_date
+)
+SELECT customer_id, COUNT(product_name) AS total_item, SUM(price) AS amount_spent
+FROM order_bf_member_cte AS ob
+GROUP BY customer_id;
 /*9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?*/
 /*10. In the first week after a customer joins the program (including their join date)
 they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?*/
