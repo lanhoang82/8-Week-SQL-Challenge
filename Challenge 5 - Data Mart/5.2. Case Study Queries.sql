7a8b2e5e927f0dd343934d6150fe71a8dd16bd67
Lan <hong-lan.hoang@etu.unige.ch>
2023-07-11 00:16:05 +0200

    Update 5.2. Case Study Queries.sql

--- Challenge 5 - Data Mart/5.2. Case Study Queries.sql
+++ Challenge 5 - Data Mart/5.2. Case Study Queries.sql
@@ -7,26 +7,43 @@ In a single query, perform the following operations and generate a new table in 
 clean_weekly_sales:
 
 1. Convert the week_date to a DATE format
-
 2. Add a week_number as the second column for each week_date value, for example any value from the 1st of 
 January to 7th of January will be 1, 8th to 14th will be 2 etc
-
 3. Add a month_number with the calendar month for each week_date value as the 3rd column
-
 4. Add a calendar_year column as the 4th column containing either 2018, 2019 or 2020 values
-
 5. Add a new column called age_band after the original segment column using the following mapping on the 
 number inside the segment value
-
 6. Add a new demographic column using the following mapping for the first letter in the segment values:
-
 7. Ensure all null string values with an "unknown" string value in the original segment column as well as the 
 new age_band and demographic columns
-
 8. Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal 
-places for each record
+places for each record*/
 
-B. Data Exploration
+DROP TABLE IF EXISTS data_mart.clean_weekly_sales;
+CREATE TABLE data_mart.clean_weekly_sales AS -- create a new table based on the result set of a SELECT query
+	SELECT TO_DATE(week_date, 'dd/mm/yy') AS week_date,
+	EXTRACT (WEEK FROM TO_DATE(week_date, 'dd/mm/yy')) AS week_number,
+	EXTRACT (MONTH FROM TO_DATE(week_date, 'dd/mm/yy')) AS month_number,
+	EXTRACT (YEAR FROM TO_DATE(week_date, 'dd/mm/yy')) AS calendar_year,
+	CASE 
+		WHEN segment LIKE '%1' THEN 'Young Adults'
+		WHEN segment LIKE '%2' THEN 'Middle Aged'
+		WHEN segment LIKE '%3' OR segment LIKE '%4' THEN 'Retirees' 
+		ELSE 'Unknown'
+		END AS age_band,
+	CASE
+		WHEN segment LIKE 'C%' THEN 'Couples'
+		WHEN segment LIKE 'F%' THEN 'Families'
+		ELSE 'Unknown'
+		END AS demographic,
+	ROUND(sales/transactions, 2) AS avg_transaction
+FROM weekly_sales;
+
+SELECT *
+FROM clean_weekly_sales
+LIMIT 5;
+
+/*B. Data Exploration
 
 1. What day of the week is used for each week_date value?
 2. What range of week numbers are missing from the dataset?
