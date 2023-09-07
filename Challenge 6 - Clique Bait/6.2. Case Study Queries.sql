0a948736d599a816ce629ff34a366f3d96b28da8
Lan <hong-lan.hoang@etu.unige.ch>
2023-09-07 15:38:47 +0200

    Week 6 progress

--- Challenge 6 - Clique Bait/6.2. Case Study Queries.sql
+++ Challenge 6 - Clique Bait/6.2. Case Study Queries.sql
@@ -1,49 +1,70 @@
 /*2. Digital Analysis
-Using the available datasets - answer the following questions using a single query for each one:
+Using the available datasets - answer the following questions using a single query for each one:*/
 
-1. How many users are there?
-2. How many cookies does each user have on average?
-3. What is the unique number of visits by all users per month?
-4. What is the number of events for each event type?
-5. What is the percentage of visits which have a purchase event?
-6. What is the percentage of visits which view the checkout page but do not have a purchase event?
-7. What are the top 3 pages by number of views?
-8. What is the number of views and cart adds for each product category?
-9. What are the top 3 products by purchases?
+SELECT * FROM clique_bait.events;
 
-3. Product Funnel Analysis
-Using a single SQL query - create a new output table which has the following details:
+/*1. How many users are there?*/
 
-1. How many times was each product viewed?
-2. How many times was each product added to cart?
-3. How many times was each product added to a cart but not purchased (abandoned)?
-4. How many times was each product purchased?
-5. Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.
+SELECT COUNT(DISTINCT user_id) FROM clique_bait.users;
 
-Use your 2 new output tables - answer the following questions:
+/*2. How many cookies does each user have on average?*/
+SELECT user_id, COUNT(DISTINCT cookie_id) "num_cookie"
+FROM clique_bait.users
+GROUP BY user_id;
 
-1. Which product had the most views, cart adds and purchases?
-2. Which product was most likely to be abandoned?
-3. Which product had the highest view to purchase percentage?
-4. What is the average conversion rate from view to cart add?
-5. What is the average conversion rate from cart add to purchase?
+/*3. What is the unique number of visits by all users per month?*/
 
-3. Campaigns Analysis
-Generate a table that has 1 single row for every unique visit_id record and has the following columns:
+WITH event_by_month_cte AS (
+	SELECT visit_id, 
+	EXTRACT('MONTH' FROM event_time) "event_month"
+	FROM clique_bait.events
+)
+SELECT event_month, COUNT(DISTINCT visit_id) "unique_visit"
+FROM event_by_month_cte
+GROUP BY event_month;
+/*4. What is the number of events for each event type?*/
+/*5. What is the percentage of visits which have a purchase event?*/
+/*6. What is the percentage of visits which view the checkout page but do not have a purchase event?*/
+/*7. What are the top 3 pages by number of views?*/
+/*8. What is the number of views and cart adds for each product category?*/
+/*9. What are the top 3 products by purchases?*/
 
-- user_id
-- visit_id
-- visit_start_time: the earliest event_time for each visit
-- page_views: count of page views for each visit
-- cart_adds: count of product cart add events for each visit
-- purchase: 1/0 flag if a purchase event exists for each visit
-- campaign_name: map the visit to a campaign if the visit_start_time falls between the start_date and end_date
-- impression: count of ad impressions for each visit
-- click: count of ad clicks for each visit
-- (Optional column) cart_products: a comma separated text value with products added to the cart sorted by the order they were added to the cart (hint: use the sequence_number)
-Use the subsequent dataset to generate at least 5 insights for the Clique Bait team - bonus: prepare a single A4 infographic that the team can use for their management reporting sessions, be sure to emphasise the most important points from your findings.
+/*3. Product Funnel Analysis
+Using a single SQL query - create a new output table which has the following details:*/
 
-Some ideas you might want to investigate further include:
+/*1. How many times was each product viewed?*/
+/*2. How many times was each product added to cart?*/
+/*3. How many times was each product added to a cart but not purchased (abandoned)?*/
+/*4. How many times was each product purchased?*/
+/*5. Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.*/
+
+/*Use your 2 new output tables - answer the following questions:*/
+
+/*1. Which product had the most views, cart adds and purchases?*/
+/*2. Which product was most likely to be abandoned?*/
+/*3. Which product had the highest view to purchase percentage?*/
+/*4. What is the average conversion rate from view to cart add?*/
+/*5. What is the average conversion rate from cart add to purchase?*/
+
+/*3. Campaigns Analysis
+Generate a table that has 1 single row for every unique visit_id record and has the following columns:*/
+
+/*- user_id*/
+/*- visit_id*/
+/*- visit_start_time: the earliest event_time for each visit*/
+/*- page_views: count of page views for each visit*/
+/*- cart_adds: count of product cart add events for each visit*/
+/*- purchase: 1/0 flag if a purchase event exists for each visit*/
+/*- campaign_name: map the visit to a campaign if the visit_start_time falls between the start_date and end_date*/
+/*- impression: count of ad impressions for each visit*/
+/*- click: count of ad clicks for each visit*/
+/*- (Optional column) cart_products: a comma separated text value with products added to the cart sorted 
+by the order they were added to the cart (hint: use the sequence_number)
+Use the subsequent dataset to generate at least 5 insights for the Clique Bait team - bonus: prepare a 
+single A4 infographic that the team can use for their management reporting sessions, be sure to 
+emphasise the most important points from your findings.*/
+
+/*Some ideas you might want to investigate further include:
 
 - Identifying users who have received impressions during each campaign period and comparing each metric with other users who did not have an impression event
 - Does clicking on an impression lead to higher purchase rates?
