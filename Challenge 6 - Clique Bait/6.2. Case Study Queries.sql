232115969d249dfaea4116e09be5c5fd2948434e
Lan <hong-lan.hoang@etu.unige.ch>
2023-09-18 14:26:52 +0200

    Update 6.2. Case Study Queries.sql

--- Challenge 6 - Clique Bait/6.2. Case Study Queries.sql
+++ Challenge 6 - Clique Bait/6.2. Case Study Queries.sql
@@ -1,7 +1,8 @@
 /*2. Digital Analysis
 Using the available datasets - answer the following questions using a single query for each one:*/
 
-SELECT * FROM clique_bait.events;
+SELECT * FROM clique_bait.events
+WHERE visit_id = 'fbfdcb';
 
 /*1. How many users are there?*/
 
@@ -117,15 +118,143 @@ Using a single SQL query - create a new output table which has the following det
 /*2. How many times was each product added to cart?*/
 /*3. How many times was each product added to a cart but not purchased (abandoned)?*/
 /*4. How many times was each product purchased?*/
+
+CREATE TABLE funnel_analysis AS
+
+WITH page_view_cte AS(
+SELECT page_name, COUNT(visit_id) "page_view_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Page View'
+	GROUP BY page_name
+),
+last_step_purchase_cte AS(
+SELECT visit_id, MAX(sequence_number) "last_step_purchase"
+FROM clique_bait.events
+	WHERE event_type = 3
+GROUP BY visit_id
+),
+
+add_cart_cte AS(
+SELECT page_name, COUNT(clique_bait.events.visit_id) "add_cart_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Add to Cart'
+	GROUP BY page_name
+),
+
+cart_purchased_cte AS(
+SELECT page_name, COUNT(clique_bait.events.visit_id) "add_cart_purchased_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	INNER JOIN last_step_purchase_cte
+		ON last_step_purchase_cte.visit_id = clique_bait.events.visit_id -- visit_ids that resulted in purchase at the last step
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Add to Cart'
+	GROUP BY page_name
+)
+		
+SELECT page_view_cte.page_name, page_view_count, add_cart_count, add_cart_purchased_count,
+	add_cart_count - add_cart_purchased_count "cart_abandoned_count",
+	ROUND((add_cart_purchased_count::numeric / page_view_count::numeric) * 100, 2) "view_to_purchase_pct"
+FROM page_view_cte
+	JOIN add_cart_cte
+		ON page_view_cte.page_name = add_cart_cte.page_name
+	JOIN cart_purchased_cte
+		ON page_view_cte.page_name = cart_purchased_cte.page_name;
+
 /*5. Additionally, create another table which further aggregates the data for the above points but this time for each product category instead of individual products.*/
 
+CREATE TABLE funnel_analysis_prod_cat AS
+
+WITH page_view_cte AS(
+SELECT product_category, COUNT(visit_id) "page_view_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Page View'
+	GROUP BY product_category
+),
+last_step_purchase_cte AS(
+SELECT visit_id, MAX(sequence_number) "last_step_purchase"
+FROM clique_bait.events
+	WHERE event_type = 3
+GROUP BY visit_id
+),
+
+add_cart_cte AS(
+SELECT product_category, COUNT(clique_bait.events.visit_id) "add_cart_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Add to Cart'
+	GROUP BY product_category
+),
+
+cart_purchased_cte AS(
+SELECT product_category, COUNT(clique_bait.events.visit_id) "add_cart_purchased_count"
+	FROM clique_bait.events
+	LEFT JOIN clique_bait.event_identifier
+		ON clique_bait.events.event_type = clique_bait.event_identifier.event_type
+	LEFT JOIN clique_bait.page_hierarchy
+		ON clique_bait.events.page_id = clique_bait.page_hierarchy.page_id
+	INNER JOIN last_step_purchase_cte
+		ON last_step_purchase_cte.visit_id = clique_bait.events.visit_id -- visit_ids that resulted in purchase at the last step
+	WHERE product_category IS NOT NULL 
+	 	AND event_name = 'Add to Cart'
+	GROUP BY product_category
+)
+		
+SELECT page_view_cte.product_category, page_view_count, add_cart_count, add_cart_purchased_count,
+	add_cart_count - add_cart_purchased_count "cart_abandoned_count"
+FROM page_view_cte
+	JOIN add_cart_cte
+		ON page_view_cte.product_category = add_cart_cte.product_category
+	JOIN cart_purchased_cte
+		ON page_view_cte.product_category = cart_purchased_cte.product_category;
+		
 /*Use your 2 new output tables - answer the following questions:*/
 
 /*1. Which product had the most views, cart adds and purchases?*/
+
+-- Oyster has the most views,
+-- Lobster has the most cart adds,
+-- Lobster also has the highest purchases
+
+-- Overall lobster seems to be doing quite well as a product. 
+
 /*2. Which product was most likely to be abandoned?*/
+-- Russian caviar is most likely to be abandoned after being added to cart. 
+
 /*3. Which product had the highest view to purchase percentage?*/
+-- Lobster has the highest view to purchase percentage, at 48.74%
+
 /*4. What is the average conversion rate from view to cart add?*/
+SELECT 
+	ROUND(AVG((add_cart_count::numeric/page_view_count::numeric)*100),2) "avg_view_2_cart_rate"
+FROM funnel_analysis
+
 /*5. What is the average conversion rate from cart add to purchase?*/
+SELECT 
+	ROUND(AVG((add_cart_purchased_count::numeric/add_cart_count::numeric)*100),2) "avg_cart_2_purchase"
+FROM funnel_analysis
 
 /*3. Campaigns Analysis
 Generate a table that has 1 single row for every unique visit_id record and has the following columns:*/
